c88051b2d08c25f57dd4eeca047b15e9ca700287
Iby tresa augustine <ibytresa3@gmail.com>
2021-02-05 02:17:35 +0530

    my first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule }   from '@angular/forms';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+//import { SavetagsComponent } from './savetags/savetags.component';
+import { NoteCardsComponent } from './note-cards/note-cards.component';
+import { NoteDetailsComponent } from './note-details/note-details.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NoteDetailsComponent,
+  //  SavetagsComponent,
+    NoteCardsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    
+    AppRoutingModule,
+    FormsModule  ,
   ],
   providers: [],
   bootstrap: [AppComponent]
